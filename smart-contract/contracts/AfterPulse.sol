99c7527b30cef6fbfc2d684589a5c17885e73e2b
Anand Singh <anandteam9777@gmail.com>
2025-10-16 22:26:37 +0530

    Add SmartContract Code

---
+++ smart-contract/contracts/AfterPulse.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier:MIT 
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+
+contract PulseTrackcore is ReentrancyGuard , Ownable , Pausable {
+    using SafeERC20 for IERC20 ;
+
+    struct Nominee {
+
+        address wallet ;
+        uint256 share ;
+    }
+      struct LockInfo {
+        uint256 amount;
+        uint256 startTime;
+        uint256 lockPeriod;
+        bool active;
+        bool zkpVerified;
+        Nominee[] nominees;
+        uint256 gracePeriod;
+        uint256 lastActivity;
+        address token; 
+        uint256 inactivityPeriod;
+    }
+
+   
+}
